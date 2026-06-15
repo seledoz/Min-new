890062b4e2382f9781913fa7dae516baecb6d936
vinaoooooz <vl.molina@hotmail.com>
2026-06-15 20:36:10 -0300

    expoe skipTarget e isTargetSkipped no auto-attack para romper ciclo chase/give-up

--- src/modules/auto-attack.js
+++ src/modules/auto-attack.js
@@ -797,6 +797,8 @@ window.__minibiaBotBundle.installAutoAttackModule = function installAutoAttackMo
     isCombatActive,
     syncMeleeChase,
     normalizeHotbarSlot,
+    skipTarget,
+    isTargetSkipped,
     config,
   };
 };
