4297a9fa6eff3c56c02ec2dbda5ec86f65a2b23b
seledoz <donavonpfeifer@hotmail.com>
2026-07-06 00:48:37 -0600

    Include AoE spell and red text alert in build

--- build.sh
+++ build.sh
@@ -25,6 +25,8 @@ cat \
   src/modules/auto-invisible.js \
   src/modules/auto-magic-shield.js \
   src/modules/auto-attack.js \
+  src/modules/auto-attack-aoe.js \
+  src/modules/red-text-alert.js \
   src/modules/cave.js \
   src/modules/equip-ring.js \
   src/modules/auto-eat.js \
