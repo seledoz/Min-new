781f376b47d10d997b53fcfe1306b93d70510486
seledoz <donavonpfeifer@hotmail.com>
2026-07-06 04:01:16 -0600

    Include cave arrow key pathing in build

--- build.sh
+++ build.sh
@@ -30,6 +30,7 @@ cat \
   src/modules/red-text-alert.js \
   src/modules/cave.js \
   src/modules/cave-forward-loop.js \
+  src/modules/cave-arrow-keys.js \
   src/modules/equip-ring.js \
   src/modules/auto-eat.js \
   src/modules/talk.js \
