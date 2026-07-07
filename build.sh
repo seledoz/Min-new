51400286423b4af895345f32a5d0f204fa64063a
seledoz <donavonpfeifer@hotmail.com>
2026-07-06 20:12:53 -0600

    include cave waypoint actions in build

--- build.sh
+++ build.sh
@@ -31,6 +31,7 @@ cat \
   src/modules/cave.js \
   src/modules/cave-forward-loop.js \
   src/modules/cave-arrow-keys.js \
+  src/modules/cave-waypoint-actions.js \
   src/modules/waypoint-profiles.js \
   src/modules/equip-ring.js \
   src/modules/auto-eat.js \
