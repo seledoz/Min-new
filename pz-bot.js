b99136319146e908d2babf4a44ca350463d82547
seledoz <donavonpfeifer@hotmail.com>
2026-07-14 04:04:15 -0600

    Load 100ms rune retry module

--- pz-bot.js
+++ pz-bot.js
@@ -17,6 +17,7 @@
     "src/modules/auto-attack.js",
     "src/modules/auto-attack-priority.js",
     "src/modules/auto-attack-rune-cooldown.js",
+    "src/modules/auto-attack-rune-retry.js",
     "src/modules/auto-attack-block-follow-while-targeted.js",
     "src/modules/auto-attack-aoe.js",
     "src/modules/auto-attack-gfb.js",
